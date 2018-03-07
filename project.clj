147ce0b5a8cf1e4e85bad3e2c617f8ad9cb205b2
icylisper <icylisper@gmail.com>
2018-03-06 17:37:21 -0800

    update ci config
    - remove project.clj

--- project.clj
+++
@@ -1,9 +0,0 @@
-(def VERSION (or (not-empty (System/getenv "SEMVER")) "0.0.1-SNAPSHOT"))
-
-(defproject net.omnypay/nomad VERSION
-  :encoding "utf-8"
-  :description "Nomad is the OmnyPay schema migration library"
-  :dependencies [[com.taoensso/timbre "4.3.1"]
-                 [org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.0"]
-                 [com.stuartsierra/dependency "0.2.0"]])
